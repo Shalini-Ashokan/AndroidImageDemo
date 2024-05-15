0298a97adac73320cb81deebb5bacd0d202c0972
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2024-05-15 19:05:10 +0530

    Demo for Image

---
+++ ImageDemo/ImageDemo/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+
+namespace ImageDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        private HttpClient httpClient;
+        public MainPage()
+        {
+            InitializeComponent();
+            httpClient = new HttpClient();
+            this.LoadImage();
+        }
+
+        private async void LoadImage()
+        {
+            var imageBytes = await httpClient.GetByteArrayAsync("https://dummyimage.com/300x200/000/fff.png");
+            MemoryStream cacheStream = new(imageBytes);
+            MemoryStream memoryStream = new(imageBytes);
+            if (ImageView != null)
+            {
+                ImageView.Source = ImageSource.FromStream(() => memoryStream);
+            }
+        }
+    }
+
+}
---
+++ ImageDemo/ImageDemo/Page1.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ImageDemo
+{
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
